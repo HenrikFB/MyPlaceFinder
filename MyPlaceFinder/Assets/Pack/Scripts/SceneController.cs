c6d095759cb4570d12133594aca19f13c7f6e12b
HenrikFB <henrik_1993@hotmail.com>
2019-01-14 08:40:19 +0100

    Creating sceneController. Before adding gps.

---
+++ MyPlaceFinder/Assets/Pack/Scripts/SceneController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour
+{
+
+    public static string keyword;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void askGoogle(string PassedKeyword)
+    {
+        keyword = PassedKeyword;
+    }
+
+    public void changeScene(string scene) {
+        SceneManager.LoadSceneAsync(scene);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
